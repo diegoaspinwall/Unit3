66253e80fbcb6719af3a9c4cdd26c3b6945f2275
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-27 13:45:41 -0400

    Updated from Brython Server: 9/27/2017 1:45:41 PM

--- loopDemo.py
+++ loopDemo.py
@@ -42,10 +42,11 @@ for i in range(1,6):
     total = total + i
 print(total)
 """
-
+"""
 total = 0
 i = 1
 while i<=5:
-    total = total+i
-    i = i+1
+    total += i
+    i += 1
 print(total)
+"""

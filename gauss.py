e7ae77a9f33cebb62c0290e17074a31727cefafd
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-28 14:32:28 -0400

    Updated from Brython Server: 9/28/2017 2:32:28 PM

--- gauss.py
+++ gauss.py
@@ -1,1 +1,13 @@
+#Diego Aspinwall
+#9-28-17
+#gauss.py
 
+lower = int(input('Enter lower number: '))
+higher = int(input('Enter upper number: '))
+dist = int(input('Enter the difference between each number: '))
+
+total = lower
+
+for i in range(lower,higher+1,dist):
+    total = total + i
+print('The sum of the numbers from', lower, 'to', higher, 'with a difference of',dist, 'is', total)

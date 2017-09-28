37cf141ca48c16a66645058cf27ba60276032d11
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-28 14:50:20 -0400

    Updated from Brython Server: 9/28/2017 2:50:20 PM

--- converter2.py
+++ converter2.py
@@ -10,25 +10,25 @@ while True:
     print('5) "quit"')
     print(' ')
 
-    num = int(input('Choose a number: '))
+    num = input('Choose an option: ')
 
 
-    if num==4:
+    if num=='4':
         cel = float(input('Enter Degrees in Celsius: '))
         print(cel, 'degrees Celsius is', cel*9/5 + 32, 'degrees in Fahrenheit')
         print(' ')
         print(' ')
-    elif num==3:
+    elif num=='3':
         lit = float(input('Enter Volume in Liters: '))
         print(lit, 'Liters is', lit*0.264172, 'Gallons')
         print(' ')
         print(' ')
-    elif num==2:
+    elif num=='2':
         mass = float(input('Enter Mass in Kilograms: '))
         print(mass, 'Kilograms is', mass*2.20462, 'Pounds')
         print(' ')
         print(' ')
-    elif num==1:
+    elif num=='1':
         kilo = float(input('Enter Distance in Kilometers: '))
         print(kilo, 'Kilometers is', kilo*0.621371, 'Miles')
         print(' ')
@@ -36,6 +36,6 @@ while True:
     elif num=='quit':
         break
     else:
-        print('Please check number BBBOIII')
+        print('Please check spelling BBBOIII')
         print(' ')
         print(' ')

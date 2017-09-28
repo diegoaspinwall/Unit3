0a418e03caf5540899e44b2cd62aef30036f5e44
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-28 14:37:15 -0400

    Updated from Brython Server: 9/28/2017 2:37:14 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -3,7 +3,7 @@
 #hitchhiker.py
 
 while True:
-    text = input('Ask a question or type"quit": ')
+    text = input('Ask a question or type "quit": ')
     if text == 'quit':
         break
     print('42')

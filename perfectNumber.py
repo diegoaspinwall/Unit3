732ddd6ea49ce4df37f820568f9e7f8d2394f0bf
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-29 11:41:59 -0400

    Updated from Brython Server: 9/29/2017 11:42:00 AM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,16 @@
+#Diego Aspinwall
+#9-29-17
+#perfectNumber.py
 
+guess = int(input('Enter a number: '))
+total = 0
+
+i=0
+while i<num:
+    i+=1
+    if num%i==0:
+        total+=i
+    if total==guess:
+        print('Perfect')
+    else:
+        print('Not Perfect')

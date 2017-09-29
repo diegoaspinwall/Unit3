523e62d68daed4bb51c44d389021c792a211ea15
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-29 12:57:27 -0400

    Updated from Brython Server: 9/29/2017 12:57:28 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -4,10 +4,14 @@
 
 guess = int(input('Enter a number: '))
 total = 0
-
+"""
 i=1
 while i<=guess:
     i+=1
+    if guess%i==0:
+        total+=i
+"""
+for i in range(2,guess):
     if guess%i==0:
         total+=i
 if total==guess:

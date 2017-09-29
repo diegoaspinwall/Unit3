7540938e72fa6a5d2ad0d435a69a04ba8efb169d
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-29 14:39:34 -0400

    Updated from Brython Server: 9/29/2017 2:39:35 PM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,17 @@
+#Diego Aspinwall
+#9-29-17
+#fri13.py
 
+from calendar import weekday
+from datetime import date
+
+yNow = date.today().year
+mNow = date.today().month
+dNow = date.today().day
+wdnow = weekday(year,month,day)
+
+i=0
+
+while i<=10:
+    
+print(yNow, mNow, dNow, wdnow)

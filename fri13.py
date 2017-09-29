cd6257785c0da7e2cc35f0332bd04490145b684e
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-29 15:11:10 -0400

    Updated from Brython Server: 9/29/2017 3:11:11 PM

--- fri13.py
+++ fri13.py
@@ -17,10 +17,12 @@ while i<=10:
     if weekday(yNow,mNow,13)==4:
         print(mNow, '13', yNow)
         i+=1
-    while m<=12:
-        m+=1
-        if weekday(yNow, mNow+m,13)==4:
-            print(mNow+m, '13', yNow)
-            
+    while y<10:
+        while m<=12:
+            m+=1
+            if weekday(yNow, mNow+m,13)==4:
+                print(mNow+m, '13', yNow)
+        y+=1
+    
 
 print(yNow, mNow, dNow, wdnow)

75510f35721d0191c1975c90c15e9b5a28c10a30
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-02 10:19:41 -0400

    Updated from Brython Server: 10/2/2017 10:19:41 AM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -1,1 +1,15 @@
+#Diego Aspinwall
+#10-2-17
+#changeComputerLoop.py
+
+centtotal = int(input('Input number of cents: '))
+quarters = centtotal//25
+dimes = (centtotal-quarters*25)//10
+nickles = (centtotal-(dimes*10+quarters*25))//5
+pennies = (centtotal-(nickles*5+dimes*10+quarters*25))
+
+print('Quarters: ', quarters)
+print('Dimes: ', dimes)
+print('Nickles: ', nickles)
+print('Pennies: ', pennies)
 

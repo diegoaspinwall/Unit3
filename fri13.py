87e62b03ce724c96d33625c4efca95f64edcfce9
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-01 21:41:37 -0400

    Updated from Brython Server: 10/1/2017 9:41:37 PM

--- fri13.py
+++ fri13.py
@@ -10,16 +10,16 @@ mNow = date.today().month
 dNow = date.today().day
 wdnow = weekday(yNow,mNow,dNow)
 
-y = 0
-m = 0
+y = yNow
+m = mNow
 i=0
 while i<=10:
     if weekday(yNow,mNow,13)==4 and dNow<13:
         print(mNow, '13', yNow)
         i+=1
-    while mNow+m<=12:
+    while m<=12:
         m+=1
-        if weekday(yNow+y, mNow+m,13)==4:
+        if weekday(y,m,13)==4:
             print(mNow+m, '13', yNow)
             i+=1
     m=0

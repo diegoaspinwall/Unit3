eadf24d6afeb130c72b9e974f26457d182a49298
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-01 21:56:54 -0400

    Updated from Brython Server: 10/1/2017 9:56:54 PM

--- fri13.py
+++ fri13.py
@@ -19,13 +19,17 @@ if weekday(yNow,mNow,13)==4 and dNow<13:
     i+=1
 
 while i<=10:
-    while m<=12:
+    if m<=12:
         m+=1
+        """print('got here')
+        break"""
         if weekday(y,m,13)==4:
             print(mNow+m, '13', yNow)
             i+=1
-    y+=1
-    m=0
+            #didn't get here
+    else:
+        y+=1
+        m=0
     
 
 print(yNow, mNow, dNow, wdnow)

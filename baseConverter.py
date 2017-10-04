64817db2f5e8b3868afeaff8ca11a347f63a55fa
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-04 12:28:56 -0400

    Updated from Brython Server: 10/4/2017 12:28:56 PM

--- baseConverter.py
+++ baseConverter.py
@@ -7,14 +7,15 @@ from math import log10
 num = int(input('Enter a base-10 number: '))
 base = int(input('What base would you like to convert it to: '))
 
+num2=num
 power = 0
 ans = 0
 while num>0:
     ans += (num//base**power)%base
-    num= num-ans**power
+    num = num-ans**base
     power+=1
     print(ans)
 
 #print(ans)
-print(num, 'in base-',base, 'is', )
+print(num2, 'in base-',base, 'is', )
 print(totaldig)

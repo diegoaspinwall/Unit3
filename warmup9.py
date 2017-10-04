426e36a1d6d63ce14640851ffda9fcc6e0fd1369
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-04 13:43:40 -0400

    Updated from Brython Server: 10/4/2017 1:43:40 PM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,12 @@
+#Diego Aspinwall
+#10-4-17
+#warmup9.py
+
+word = input('Enter a word: ')
+
+for ch in word:
+    if ch==a or ch==e or ch==i or ch==o or ch==u:
+        print(ch.upper())
+    else:
+        print(ch)
 

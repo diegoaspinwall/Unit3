4224df1f29fb274aad3cd984e12b73b7e190d11e
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-04 13:34:12 -0400

    Updated from Brython Server: 10/4/2017 1:34:11 PM

--- betterAdditionGameDemo.py
+++ betterAdditionGameDemo.py
@@ -1,1 +1,13 @@
+#Diego Aspinwall
+#10-4-17
+#betterAdditionGameDemo.py - ask addition problems until user gets 5
 
+from random import randint
+
+numCorrect=0
+while numCorrect<5:
+    num1 = randint(-10,10)
+    num2 = randint(-10,10)
+    question = 'What is '+str(num1) + '+' +str(num2)+'?'
+    answer = int(input(question))
+    break

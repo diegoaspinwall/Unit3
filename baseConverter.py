d74cf3f99b9d3888e670bea1cc22bfa60b020aaf
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-05 10:11:40 -0400

    Updated from Brython Server: 10/5/2017 10:11:40 AM

--- baseConverter.py
+++ baseConverter.py
@@ -2,8 +2,6 @@
 #10-2-17
 #baseConverter.py
 
-from math import log10
-
 num = int(input('Enter a base-10 number: '))
 base = int(input('What base would you like to convert it to: '))
 
@@ -12,9 +10,22 @@ power = 0
 ans = 0
 while num>0:
     ans += (num//base**power)%base
-    num += -ans
+    num = num-ans
     power += 1
-    print(ans)
+    if ans == 10:
+        print('A')
+    elif ans == 11:
+        print('B')
+    elif ans == 12:
+        print('C')
+    elif ans == 13:
+        print('D')
+    elif ans == 14:
+        print('E')
+    elif ans == 15:
+        print('F')
+    else:
+        print(ans)
     ans=0
 
 print(num2, 'in base-',base, 'is', )

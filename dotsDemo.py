806503f12e1cdc54d01b438edf8b6d97f81a1831
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-05 14:24:33 -0400

    Updated from Brython Server: 10/5/2017 2:24:33 PM

--- dotsDemo.py
+++ dotsDemo.py
@@ -1,1 +1,12 @@
+#Diego Aspinwall
+#10-5-17
+#dotsDemo.py - Making some dots
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+
+dot = CircleAsset(20, LineStyle(1,red), red)
+
+Sprite(dot)
+App().run()

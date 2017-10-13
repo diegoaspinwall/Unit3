fabff406410b99efa223007e48b09ffc0f5d11be
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-12 22:17:45 -0400

    Updated from Brython Server: 10/12/2017 10:17:45 PM

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,25 @@
+#Diego Aspinwall
+#10-12-17
+#madlibs
+
+person = input('Person: ')
+place = input('Place: ')
+adjective = input('Adjective: ')
+pnoun = input('Plural Noun: ')
+adjective2 = input('Adjective: ')
+pnoun2 = input('Plural Noun: ')
+place2 = input('Place: ')
+actverb = input('Action Verb: ')
+pnoun3 = input('Plural Noun: ')
+pnoun4 = input('Plural Noun: ')
+noun = input('Noun: ')
+actverb2 = input('Action Verb: ')
+noun2 = input('Noun: ')
+adjective3 = input('Adjective: ')
+
+print(' ')
+print('Last summer, my Mom and Dad took me and', person, 'on a trip to', place, '. The weather there is very', 
+adjective, '! Northern', place, 'has many', pnoun, 'and they make', adjective2, pnoun2, 'there. Many people also go to',
+place2, 'to', actverb, 'or see the', pnoun3, '. The people that live there love to eat', pnoun4, 'and are very proud of their big',
+noun, '. They also like to', actverb2, 'in the sun and swim in the', noun, '! It was a really', adjective2, 'trip!')
 
